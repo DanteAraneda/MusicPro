d696a644071d1d76c2e2ae442577151346b94eb7
daaraneda <dant.araneda@duocuc.cl>
2022-05-24 14:49:21 -0400

    problemas con crear cuenta solicionado

--- crearcuenta.php
+++ crearcuenta.php
@@ -1,4 +1,56 @@
-<?php include("template/cabecera.php"); ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crear Cuenta</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+
+
+    <a class="navbar-brand" href="login.php">MUSIC PRO</a>
+
+
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.php">Inicio
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="instrumentos.php">Productos</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="nosotros.php">Nosotros</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="listacarro.php">Carro</a>
+        </li>
+
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="text" placeholder="Buscar">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<div class="container">
+    <br/>
+        <div class="row">
 
 <?php 
 
